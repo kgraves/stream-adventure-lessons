2ee3f8126c587116579794793a0d53e2be11bf76
kg <kylgrvs@gmail.com>
2015-12-11 15:46:49 -0800

    completes step 5

--- program.js
+++ program.js
@@ -1,7 +1,16 @@
+var split = require('split');
 var through = require('through2');
-var tr = through(function(buffer, encoding, next) {
-  this.push(buffer.toString('utf8').toUpperCase());
-  next();
-});
+var index = 1;
 
-process.stdin.pipe(tr).pipe(process.stdout);
+process.stdin
+    .pipe(split())
+    .pipe(through(function(line, encoding, next) {
+      if (index % 2 === 0) {
+        this.push(line.toString('utf8').toUpperCase() + '\n');
+      } else {
+        this.push(line.toString('utf8').toLowerCase() + '\n');
+      }
+
+      index += 1;
+      next();
+    })).pipe(process.stdout);
