b363d6ce92359c497af6267ba7cd5be243566bf7
kg <kylgrvs@gmail.com>
2015-12-11 17:10:53 -0800

    completes step 12

--- program.js
+++ program.js
@@ -1,7 +1,17 @@
-var spawn = require('child_process').spawn;
 var duplex = require('duplexer2');
+var through = require('through2').obj;
 
-module.exports = function(cmd, args) {
-  var proc = spawn(cmd, args);
-  return duplex(proc.stdin, proc.stdout);
+module.exports = function(counter) {
+  var counts = {};
+  var inStream = through(function(row, encoding, next) {
+    // increment count
+    counts[row.country] = (counts[row.country] || 0) + 1;
+    next();
+  }, function(done) {
+    // set countrs
+    counter.setCounts(counts)
+    done();
+  });
+
+  return duplex(inStream, counter);
 };
