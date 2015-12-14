26775a667ea7340c34a881b9b9861ff6daf03415
kg <kylgrvs@gmail.com>
2015-12-14 13:11:09 -0800

    completes step 13

--- program.js
+++ program.js
@@ -1,17 +1,39 @@
-var duplex = require('duplexer2');
-var through = require('through2').obj;
+var combine = require('stream-combiner');
+var split = require('split');
+var thru = require('through2');
+var zlib = require('zlib');
 
-module.exports = function(counter) {
-  var counts = {};
-  var inStream = through(function(row, encoding, next) {
-    // increment count
-    counts[row.country] = (counts[row.country] || 0) + 1;
+module.exports = function () {
+  var currentGenre;
+  var tr = thru(function(line, _, next) {
+    if (line.length <= 0) {
+      return next();
+    }
+
+    var row = JSON.parse(line);
+    if (row.type === 'genre') {
+      if (currentGenre) {
+        this.push(JSON.stringify(currentGenre) + '\n');
+      }
+      
+      currentGenre = {
+        name: row.name,
+        books: []
+      };
+    } else if (row.type === 'book') {
+      currentGenre.books.push(row.name);
+    }
+
     next();
   }, function(done) {
-    // set countrs
-    counter.setCounts(counts)
+    // if it ends with a genre, push that genre
+    this.push(JSON.stringify(currentGenre) + '\n');
     done();
   });
 
-  return duplex(inStream, counter);
+  return combine(
+    split(),
+    tr,
+    zlib.createGzip()
+  );
 };
