698986a5d527e1912b01c5f2aa157cbc675afacd
kg <kylgrvs@gmail.com>
2015-12-14 13:18:17 -0800

    completes step 14

--- program.js
+++ program.js
@@ -1,39 +1,8 @@
-var combine = require('stream-combiner');
-var split = require('split');
-var thru = require('through2');
-var zlib = require('zlib');
+var crypto = require('crypto');
 
-module.exports = function () {
-  var currentGenre;
-  var tr = thru(function(line, _, next) {
-    if (line.length <= 0) {
-      return next();
-    }
+var passphrase = process.argv[2];
+var stream = crypto.createDecipher('aes256', passphrase);
 
-    var row = JSON.parse(line);
-    if (row.type === 'genre') {
-      if (currentGenre) {
-        this.push(JSON.stringify(currentGenre) + '\n');
-      }
-      
-      currentGenre = {
-        name: row.name,
-        books: []
-      };
-    } else if (row.type === 'book') {
-      currentGenre.books.push(row.name);
-    }
-
-    next();
-  }, function(done) {
-    // if it ends with a genre, push that genre
-    this.push(JSON.stringify(currentGenre) + '\n');
-    done();
-  });
-
-  return combine(
-    split(),
-    tr,
-    zlib.createGzip()
-  );
-};
+process.stdin
+    .pipe(stream)
+    .pipe(process.stdout);
