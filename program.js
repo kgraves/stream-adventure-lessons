bdd6415f324497940956d8c29485b0150db104e6
kg <kylgrvs@gmail.com>
2015-12-14 13:41:39 -0800

    completes step 15

--- program.js
+++ program.js
@@ -1,8 +1,25 @@
+var concat = require('concat-stream');
 var crypto = require('crypto');
+var tar = require('tar');
+var thru = require('through2');
+var zlib = require('zlib');
 
-var passphrase = process.argv[2];
-var stream = crypto.createDecipher('aes256', passphrase);
+var cipherName = process.argv[2];
+var passphrase = process.argv[3];
+var decipherStream = crypto.createDecipher('aes256', passphrase);
+
+var tarParser = tar.Parse();
+tarParser.on('entry', function (entry) {
+  if (entry.type == 'File') {
+    entry
+        .pipe(crypto.createHash('md5', { encoding: 'hex' }))
+        .pipe(concat(function(hex) {
+          console.log(hex + ' ' + entry.path);
+        }));
+  }
+});
 
 process.stdin
-    .pipe(stream)
-    .pipe(process.stdout);
+    .pipe(crypto.createDecipher(cipherName, passphrase))
+    .pipe(zlib.createGunzip())
+    .pipe(tarParser);
